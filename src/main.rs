cb17d34fcd469f6de403b2659778e5bcfd56c68a
Raghav <goelr2007@gmail.com>
2026-02-09 23:04:17 -0500

    value struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::ops::{Add, Mul};
+
+#[derive(Debug)]
+struct Value {
+    value: f64,
+    children: Vec<Value>,
+}
+
+impl Add<Value> for Value {
+    type Output = Value;
+
+    fn add(self, other:Value) -> Value {
+        Value {
+            value: self.value + other.value,
+        }
+    }
+}
+
+impl Mul<Value> for Value {
+    type Output = Value;
+
+    fn mul(self, other:Value) -> Value {
+        Value {
+            value: self.value * other.value,
+        }
+    }
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Value{value: 10.0};
+    let b = Value{value: 20.0};
+    let value = a + b;
+    println!("Value: {:?}", value);
 }
